6d77ed9be7285db49357abdbb6e6e159d9e375bb
Bronson-92 <pavelfradinskiy@gmail.com>
2024-03-19 08:50:04 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.reviews__slider', {
+
+  slidesPerView: 2,
+  spaceBetween: 22,
+  loop: true,
+
+  navigation: {
+    nextEl: '.reviews__item-prev',
+    prevEl: '.reviews__item-next',
+  },
+});
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menuList.classList.toggle('menu--open');
+});
